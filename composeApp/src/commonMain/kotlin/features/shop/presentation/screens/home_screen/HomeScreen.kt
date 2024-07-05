6489ca7c9918f5a99b7c0e35649920580c846454
robert <robertnganga220@gmail.com>
2024-07-05 12:59:36 +0300

    Fetch shoes and display on homescreen

--- composeApp/src/commonMain/kotlin/features/shop/presentation/screens/home_screen/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/features/shop/presentation/screens/home_screen/HomeScreen.kt
@@ -2,28 +2,65 @@ package features.shop.presentation.screens.home_screen
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun HomeScreen(viewModel: HomeScreenViewModel) {
+
+    val uiState by viewModel.homeScreenState.collectAsState()
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Column {
-            Text(text = "Home Screen")
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+    ) {
+        Button(
+            onClick = {
+                viewModel.logout()
+            },
+            modifier = Modifier.align(Alignment.TopEnd)
+        ){
+            Text(text = "Log out")
+        }
+
+        Column(
+            modifier = Modifier.align(Alignment.Center)
+        ){
             Button(
                 onClick = {
-                    viewModel.logout()
-                }
+                    viewModel.getShoes()
+                },
+
             ){
-                Text(text = "Log out")
+                Text(text = "Get shoes")
+            }
+            Spacer(modifier = Modifier.height(24.dp))
+            when(uiState.shoesState){
+                is HomeScreenShoesState.Error -> {
+                    Text(
+                        text = (uiState.shoesState as HomeScreenShoesState.Error).errorMessage,
+                    )
+                }
+                is HomeScreenShoesState.Loading -> {
+                    CircularProgressIndicator()
+                }
+                is HomeScreenShoesState.Success -> {
+                    Text(
+                        text = (uiState.shoesState as HomeScreenShoesState.Success).shoes.toString()
+                    )
+                }
             }
         }
     }
