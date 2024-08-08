59e362322d3a7e9d998587c8d9d30e7af2b9af9f
robert <robertnganga220@gmail.com>
2024-08-08 09:32:57 +0300

    Fix room error

--- composeApp/src/commonMain/kotlin/core/data/database/NdulaDatabase.kt
+++ composeApp/src/commonMain/kotlin/core/data/database/NdulaDatabase.kt
@@ -7,7 +7,17 @@ import core.data.database.entities.CartItemEntity
 
 
 @Database(entities = [CartItemEntity::class], version = 1, exportSchema = false)
-abstract class NdulaDatabase: RoomDatabase() {
+abstract class NdulaDatabase: RoomDatabase(), DB {
     abstract fun cartDao(): CartDao
 
+    override fun clearAllTables() {
+        super.clearAllTables()
+    }
+
+}
+
+// FIXME: Added a hack to resolve below issue:
+// Class 'AppDatabase_Impl' is not abstract and does not implement abstract base class member 'clearAllTables'.
+interface DB {
+    fun clearAllTables() {}
 }
